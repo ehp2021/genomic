a4ebca6d69cdcab953ec62d7a1cdccdf247fef04
ehp2021 <emmahpaik@gmail.com>
2023-04-01 20:01:57 -0500

    adding read me

---
+++ README.md
@@ -0,0 +1,5 @@
+Download your raw data from 23andme. It is usually downloaded as a txt file. You can open this txt file in Excel and save it as a csv file. 
+
+The diabetes2_prob.py file will use the plink package to perform a logistic regression analysis and return your probability of getting diabetes type 2.
+
+This code was written with the help of ChatGPT
--- diabetes2_prob.py
+++ diabetes2_prob.py
@@ -1,35 +1,39 @@
+# PROGRESS STATUS
+import time
+import sys
+
+for i in range(100):
+    time.sleep(1)
+    sys.stdout.write("\r%d%%" % i)
+    sys.stdout.flush()
+
+# data analysis starts here
 import pandas as pd
 import numpy as np
 import subprocess
 
 # Read in SNP data from CSV file
-snp_data = pd.read_csv("snp_data.csv")
+#snp_data = pd.read_csv("EP_genome.csv")
+snp_data = pd.read_csv("EP_genome.csv", dtype={"rsid": str, "chromosome": str, "position": int, "genotype": str})
 
 # Convert SNP data to PLINK format
 plink_data = snp_data.copy()
-plink_data.iloc[:,2:] = plink_data.iloc[:,2:].replace({0: "A A", 1: "A B", 2: "B B"})
+plink_data.iloc[:,3] = plink_data.iloc[:,3].replace({"AA": "A A", "AB": "A B", "BB": "B B"})
 plink_data.to_csv("plink_data.ped", sep="\t", index=False, header=False)
 
 # Create MAP file
 chromosome = "1" # chromosome number
 rs_ids = plink_data.iloc[:,0].values # SNP IDs
 positions = plink_data.iloc[:,1].values # SNP positions
-map_data = np.column_stack((chromosome, rs_ids, 0, positions))
+map_data = np.column_stack((chromosome, rs_ids, np.zeros_like(rs_ids), positions))
 pd.DataFrame(map_data, columns=["CHR", "SNP", "CM", "POS"]).to_csv("plink_data.map", sep="\t", index=False, header=False)
 
 # Convert PLINK data to binary format
 plink_cmd = "plink --file plink_data --make-bed --out plink_data"
 subprocess.call(plink_cmd, shell=True)
 
-
-#  use PLINK to perform a logistic regression analysis - diabetes 2 probability?
-
-import subprocess
-
-# Define PLINK command as a string
-plink_cmd = "plink --file my_data --pheno diabetes_pheno.txt --logistic --out plink_results"
-
-# Run PLINK command
+# Use PLINK to perform a logistic regression analysis
+plink_cmd = "plink --bfile plink_data --pheno diabetes_pheno.txt --logistic --out plink_results"
 subprocess.call(plink_cmd, shell=True)
 
 # Read in PLINK results
@@ -43,3 +47,5 @@ p_value = plink_results["P"].values[0]
 # Calculate odds ratio and probability of developing diabetes
 odds_ratio = np.exp(beta)
 prob = odds_ratio / (1 + odds_ratio)
+
+print("Diabetes 2 probability:", prob)
